e919d256de06b9504adf6c053f24729cfacbdfd5
stanaka <sergio_m_tanaka@hotmail.com>
2015-12-10 16:13:03 -0200

    Recuperação do tipo da entidade no repositório.

--- timemanager-domain/src/main/java/org/pinguin/domain/common/BasicRepositoryImpl.java
+++ timemanager-domain/src/main/java/org/pinguin/domain/common/BasicRepositoryImpl.java
@@ -1,10 +1,14 @@
 package org.pinguin.domain.common;
 
+import java.lang.reflect.ParameterizedType;
 import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 
 /**
  * Implementacao das operacoes basicas de um repositorio.
@@ -14,7 +18,7 @@ import javax.persistence.criteria.CriteriaQuery;
  * @param <T>
  * @param <I>
  */
-public class BasicRepositoryImpl<T, I> implements Repository<T, I> {
+public abstract class BasicRepositoryImpl<T, I> implements Repository<T, I> {
 
 	@PersistenceContext(unitName = "main")
 	private EntityManager entityManager;
@@ -37,20 +41,32 @@ public class BasicRepositoryImpl<T, I> implements Repository<T, I> {
 
 	@Override
 	public T retrieveById(I id) {
-		// TODO Auto-generated method stub
-		return null;
+		return entityManager.find(retrieveEntityType(), id);
 	}
 
 	@Override
 	public List<T> retrieveAll() {
-		// TODO Auto-generated method stub
-		return null;
+		Class<T> entityType = retrieveEntityType();
+
+		CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+		CriteriaQuery<T> q = cb.createQuery(entityType);
+		Root<T> c = q.from(entityType);
+		q.select(c);
+		TypedQuery<T> query = entityManager.createQuery(q);
+		List<T> results = query.getResultList();
+		return results;
 	}
 
 	@Override
 	public List<T> retrieveByCriteria(CriteriaQuery<T> query) {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	@SuppressWarnings("unchecked")
+	private Class<T> retrieveEntityType() {
+		Class<T> type = (Class<T>) ((ParameterizedType) getClass().getGenericSuperclass()).getActualTypeArguments()[0];
+		return type;
 	}
 
 }
